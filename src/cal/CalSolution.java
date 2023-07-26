839b35289caebd5220a72fd9588ee55d82ce942b
imgonnabe <qorlwn456123@gmail.com>
2023-07-26 16:50:20 +0900

    부족한 금액 계산

--- src/cal/CalSolution.java
+++ src/cal/CalSolution.java
@@ -7,7 +7,7 @@ public class CalSolution {
 
 	public long solution(int price, int money, int count) {
 		long answer = -1;
-		answer = (long) price * count * (count + 1) / 2 - money;
+		answer = price * count * (count + 1) / 2 - money;
 		return answer <= 0 ? 0 : answer;
 	}
 }
