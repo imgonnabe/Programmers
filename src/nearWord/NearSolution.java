bf123ff09555851b9e40aae9c09924aee8d72ddf
imgonnabe <qorlwn456123@gmail.com>
2023-07-26 17:38:51 +0900

    가까운 숫자

---
+++ src/nearWord/NearSolution.java
@@ -0,0 +1,16 @@
+package nearWord;
+
+import java.util.HashMap;
+
+public class NearSolution {
+	public int[] solution(String s) {
+		int[] answer = new int[s.length()];
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			answer[i] = i - map.getOrDefault(ch, i + 1);
+			map.put(ch, i);
+		}
+		return answer;
+	}
+}
